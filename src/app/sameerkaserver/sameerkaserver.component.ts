1cebdd141a29da39695b4f3f1caaaee02f1dfe24
kartiksharma51199 <ks583678@gmail.com>
2021-02-18 11:07:19 +0530

    sameerkaserverchange

---
+++ src/app/sameerkaserver/sameerkaserver.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sameerkaserver',
+  templateUrl: './sameerkaserver.component.html',
+  styleUrls: ['./sameerkaserver.component.css']
+})
+export class SameerkaserverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
