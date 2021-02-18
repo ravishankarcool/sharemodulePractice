0fd7127df2c5e743d58ec001ce6b3e024f393845
ravishankarcool <ravishankar61098@gmail.com>
2021-02-18 10:31:39 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ClientSideRowModelModule } from '@ag-grid-community/all-modules';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gridPractice';
+  modules = [ClientSideRowModelModule];
+
+  column = [
+    { field: 'make', sortable: true, filter: true },
+    { field: 'model', sortable: true, filter: true },
+    { field: 'price', sortable: true, filter: true }
+];
+
+row = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+];
+}
